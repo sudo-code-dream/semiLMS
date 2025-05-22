212b969ec11ddaa620a52b41cc5f9e5639dea182
kennethdean <kennethjrdean99@gmail.com>
2025-05-22 19:10:26 +0800

    update footer text

--- components/hero/Footer.tsx
+++ components/hero/Footer.tsx
@@ -68,7 +68,7 @@ export default function Footer() {
                 </div>
             </div>
             <div className="container mx-auto mt-8 pt-8 border-t border-gray-800 text-center text-gray-400">
-                <p>&copy; 2025 Fud00. All rights reserved.</p>
+                <p>&copy; 2025 Fud00 Tech. All rights reserved.</p>
             </div>
         </footer>
     )
