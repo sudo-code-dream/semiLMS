808d551087ec8efc7c6ce13be098481f8f91ba5a
kennethdean <kennethjrdean99@gmail.com>
2025-05-24 18:19:58 +0800

    `Update layout.tsx: add "use client" directive, import useRouter and useUser, and remove unnecessary whitespace`

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,26 +1,29 @@
-import Navbar from "./compo/Navbar"
-import Hero from "./compo/Hero"
-import Features from "./compo/Features"
-import CTA from "./compo/CTA"
-import Footer from "./compo/Footer"
+"use client";
+import Navbar from "./compo/Navbar";
+import Hero from "./compo/Hero";
+import Features from "./compo/Features";
+import CTA from "./compo/CTA";
+import Footer from "./compo/Footer";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
-    return (
-        <div className="relative min-h-screen">
-            {/* Background gradients */}
-            <div className="pointer-events-none fixed inset-0">
-                <div className="absolute inset-0 bg-gradient-to-b from-background via-background/90 to-background" />
-                <div className="absolute right-0 top-0 h-[500px] w-[500px] bg-blue-500/10 blur-[100px]" />
-                <div className="absolute bottom-0 left-0 h-[500px] w-[500px] bg-purple-500/10 blur-[100px]" />
-            </div>
+  return (
+    <div className='relative min-h-screen'>
+      {/* Background gradients */}
+      <div className='pointer-events-none fixed inset-0'>
+        <div className='absolute inset-0 bg-gradient-to-b from-background via-background/90 to-background' />
+        <div className='absolute right-0 top-0 h-[500px] w-[500px] bg-blue-500/10 blur-[100px]' />
+        <div className='absolute bottom-0 left-0 h-[500px] w-[500px] bg-purple-500/10 blur-[100px]' />
+      </div>
 
-            <div className="relative z-10">
-                <Navbar />
-                <Hero />
-                <Features />
-                <CTA />
-                <Footer />
-            </div>
-        </div>
-    )
+      <div className='relative z-10'>
+        <Navbar />
+        <Hero />
+        <Features />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  );
 }
